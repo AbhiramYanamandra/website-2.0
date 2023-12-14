a5c6379f583c22a10da76efc6bf15fc0c553ba7e
Abhiram <ysabhiram@gmail.com>
2023-12-14 12:28:47 +0530

    Beautified Single Page

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,18 +1,57 @@
 import React from "react";
 import Link from 'next/link';
-// const defaultTheme = require('tailwindcss/defaultTheme')
-
+import { FaGithub, FaLinkedin} from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
 
 export default function Page() {
-  return (
-    <>
-		<div className="h-screen w-screen bg-radial-gradient flex flex-col justify-end items-center content-center">
-			<a href='/' className="p-4 ml-5 100vh flex flex-col text-4xl font-serif opacity-70 hover:opacity-100 scale-90 hover:scale-100">Home</a>
-			<a href='/' className="p-4 ml-5 100vh flex flex-col text-4xl font-serif opacity-70 hover:opacity-100 scale-90 hover:scale-100">About</a>
-			<a href='/' className="p-4 ml-5 100vh flex flex-col text-4xl font-serif opacity-70 hover:opacity-100 scale-90 hover:scale-100">Education</a>
-			<a href='/' className="p-4 ml-5 100vh flex flex-col text-4xl font-serif opacity-70 hover:opacity-100 scale-90 hover:scale-100">Contact</a>
-		</div>
-    </>
-  );
+	return (
+		<>
+		<div className="bg-gray-900 h-screen w-screen">
+			{/* Blobs */}
+			<div className="sticky">
+				<div className="fixed top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full opacity-70 mix-blend-multiply filter blur-xl animate-blob"></div>
+				<div className="fixed top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full opacity-70 mix-blend-multiply filter blur-xl animate-blob animation-delay-2000"></div>
+				<div className="fixed -bottom-8 left-28 w-72 h-72 bg-pink-300 rounded-full opacity-70 mix-blend-multiply filter blur-xl animate-blob animation-delay-4000"></div>
+			</div>
 
+			<div className="flex flex-row ">
+				{/* Sticky Sidebar */}
+				<div className="sticky top-0 h-screen flex flex-col justify-center items-start pl-10">
+					{/* Name & Ethos Statement */}
+					<div id="home" className="justify-center content-center">
+						<a href="#home" className="text-4xl">Abhiram Yanamandra</a>
+						<p className="mt-1">Aspiring Robotics Engineer</p>
+						<p className="mt-1"> Just Do It</p>
+					</div>
+					<div className="mt-5">
+						<a href='#about' className="flex flex-col text-xl pt-1">About</a>
+						<a href='#education' className="flex flex-col text-xl pt-1">Education</a>
+						<a href='#project' className="flex flex-col text-xl pt-1">Projects</a>
+					</div>
+
+					{/* Contacts */}
+					<div id="contact" className="flex flex-row mt-5 items-baseline">
+						<div className="mr-4 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+							<a href="https://github.com/AbhiramYanamandra"><FaGithub size={"2em"} /></a>
+						</div>
+						<div className="mr-4 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+							<a href="https://www.linkedin.com/in/abhiram-yanamandra-a803b7246/"><FaLinkedin size={"2.2em"} /></a>
+						</div>
+						<div className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+							<a href="https://mail.google.com/mail/u/ysabhiram@gmail.com/#compose"><IoMdMail size={"2.2em"} /></a>
+						</div>
+					</div>
+				</div>
+
+
+				<div className="flex-grow overflow-y-auto flex flex-col items-center justify-center p-8">
+					<div id="about" className="text-4xl">ABOUT</div>
+          			<div id="education" className="text-4xl">EDUCATION</div>
+          			<div id="project" className="text-4xl">PROJECTS</div>
+        		</div>
+			</div>
+
+		</div>
+		</>
+	);
 }
--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -9,7 +9,6 @@ module.exports = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        // 'gradient-radial': 'rgba(225, 255, 0.1), 9%',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
@@ -19,13 +18,29 @@ module.exports = {
         'contain': 'contain',
         '50%': '50%',
         '16': '4rem',
-      }
-    },
-    variants: {
-      extend: {
-        scale: ['hover'], // Enable the hover variant for the scale utility
-        transformOrigin: ['hover'],
       },
+      animation: {
+        blob: 'blob 10s infinite',
+      },
+      keyframes: {
+        blob: {
+          '0%, 100%': {
+            transform: 'translate(0, 0) scale(1)',
+          },
+          '25%': {
+            transform: 'translate(20px, -50px) scale(1.1)',
+          },
+          '50%': {
+            transform: 'translate(0, 20px) scale(1)',
+          },
+          '75%': {
+            transform: 'translate(-20px, -15px) scale(0.9)',
+          },
+        },
+      },
+    },
+    variants: { 
+      extend: {},
     },
   },
   plugins: [],
